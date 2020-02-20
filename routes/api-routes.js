7acbbfe317c90c81b8f16d5cf3f87834c121e224
EMC <edwardcoad.ec@gmail.com>
2020-02-20 20:30:24 +1030

    passport auth added, route auth added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var passport = require("../config/passport");
 
 module.exports = function(app) {
   app.get("/api/event/:id", function(req, res) {
@@ -29,6 +30,31 @@ module.exports = function(app) {
   app.get("/api/charity", function(req, res) {
     // route to get data about the searched charity
     
+  });
+
+  // logging out a user
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+
+  // logging in a user
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    res.json(req.user);
+  });
+
+  // signing up a new user
+  app.post("/api/signup", function(req, res) {
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(function() {
+        res.redirect(307, "/api/login");
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
   });
 
   app.post("/api/event", function(req, res) {
