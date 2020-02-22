6ee19c487ef25d559e00f0793e9071054e32ec3a
ananya92 <1992ananya@gmail.com>
2020-02-22 19:08:30 +1030

    Edward's fixes of the routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,5 @@
 var db = require("../models");
 var passport = require("../config/passport");
-
 module.exports = function(app) {
   app.get("/api/user/:id", function(req, res) {
     db.User.findOne({
@@ -11,7 +10,6 @@ module.exports = function(app) {
       res.json(dbUser);
     })
   });
-
   app.get("/api/event/:id", function(req, res) {
     // route to get data about a particular event
     db.Events.findOne({
@@ -22,7 +20,6 @@ module.exports = function(app) {
       res.json(dbEvents);
     })
   });
-
   app.get("/api/donation/:id", function(req, res) {
     // route to get data about a particular donation
     db.Donations.findOne({
@@ -33,7 +30,6 @@ module.exports = function(app) {
       res.json(dbDonations);
     })
   });
-
   app.get("/api/charity/:id", function(req, res) {
     // route to get data about the searched charity
     db.Charity.findOne({
@@ -44,48 +40,59 @@ module.exports = function(app) {
       res.json(dbCharity);
     })
   });
-
   app.get("/api/user", function(req, res) {
     // route to get list of all events
     db.User.findAll({ }).then(function(dbUser) {
       res.json(dbUser);
     })
   });
-
   app.get("/api/event", function(req, res) {
     // route to get list of all events
     db.Events.findAll({ }).then(function(dbEvents) {
       res.json(dbEvents);
     })
   });
-
   app.get("/api/donation", function(req, res) {
     // route to get data about a particular donation
     db.Donations.findAll({ include: [db.User] }).then(function(dbDonations) { //remember to restart server after changing clauses
       res.json(dbDonations);
     })
   });
-
   app.get("/api/charity", function(req, res) {
     // route to get data about the searched charity
     db.Charity.findAll({ }).then(function(dbCharity) {
       res.json(dbCharity);
     })
   });
-
   // logging out a user
   app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
   });
-
   // logging in a user
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    res.json(req.user);
+    // console.log(req.body);
+    if(req.body.charityKey) {
+      db.User.findOne({
+        where: {
+          charityKey: req.body.charityKey
+        }
+      })
+      .then(function(dbUser) {
+        req.user.userType = "charity";
+        res.json(req.user); //might be dbUser... check later
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
+    }
+    else {
+      req.user.userType = "regular";
+      res.json(req.user);
+    }
   });
-
   // signing up a new user
-  app.post("/api/signup", function(req, res) {
+  app.post("/api/register", function(req, res) {
     db.User.create({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
@@ -94,17 +101,15 @@ module.exports = function(app) {
       password: req.body.password
     })
       .then(function() {
-        res.redirect(307, "/api/login");
+        res.redirect('/login');
       })
       .catch(function(err) {
         res.status(401).json(err);
       });
   });
-
   app.post("/api/event", function(req, res) {
     // route to POST a new event
   });
-
   app.post("/api/donation", function(req, res) {
     // route to POST a new donation
     db.Donations.create({
@@ -116,12 +121,10 @@ module.exports = function(app) {
     })
       .then(function() {
         //add a thank you message
-        res.redirect(307, "/user"); //need to fix, not finding address, maybe something to do with handlebars
+        res.redirect('/user') //need to fix, not finding address, maybe something to do with handlebars
       })
       .catch(function(err) {
         res.status(401).json(err);
       });
   });
-
 };
-
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,25 +4,22 @@
 // *********************************************************************************
 // Requiring middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
-
 // Routes
 // =============================================================
 module.exports = function(app) {
-
   // Each of the below routes just handles the HTML page that the user gets sent to.
-
   // index route loads home page
   app.get("/", function(req, res) {
     if (req.user) {
       // User has logged in
-      if(req.session.userType === "regular") {
-        res.render("user-home", {
-          layout: "user.handlebars"
-        });
-      }
-      else if(req.session.userType === "charity") {
+      if(req.user.charityKey) {
         res.render("user-home", {
           layout: "cuser.handlebars"
+        });
+      }
+      else {
+        res.render("user-home", {
+          layout: "user.handlebars"
         });
       }
     }
@@ -30,49 +27,45 @@ module.exports = function(app) {
       res.render("index");
     }
   });
-
   // user route loads home page after user logs-in
   app.get("/user", isAuthenticated, function(req, res) {
-    if(req.session.userType === "regular") {
-      res.render("user-home", {
-        layout: "user.handlebars"
-      });
-    }
-    else if(req.session.userType === "charity") {
+    console.log(req.user);
+    if(req.user.charityKey) {
       res.render("user-home", {
         layout: "cuser.handlebars"
       });
     }
+    else {
+      res.render("user-home", {
+        layout: "user.handlebars"
+      });
+    }
   });
-
   // register route loads registration page of new user 
   app.get("/register", function(req, res) {
     res.render("registration");
   });
-
   // login route loads login page for existing user
   app.get("/login", function(req, res) {
     res.render("login");
   });
-
   // donation route loads donation form for posting new donation 
   app.get("/donation", isAuthenticated, function(req, res) {
     res.render("donation", {
       layout: "user.handlebars"
     });
   });
-
   // charity search when user isn't logged in
   app.get("/charity", function(req, res) {
     if (req.user) {
-      if(req.session.userType === "regular") {
-        res.render("charitySearch", {
-          layout: "user.handlebars"
-        });
-      }
-      else if(req.session.userType === "charity") {
+      if(req.user.charityKey) {
         res.render("charitySearch", {
           layout: "cuser.handlebars"
+        });
+      }
+      else {
+        res.render("charitySearch", {
+          layout: "user.handlebars"
         });
       }
     }
@@ -80,39 +73,35 @@ module.exports = function(app) {
       res.render("charitySearch");
     }
   });
-
   // event route loads event form for posting new event
   app.get("/event", isAuthenticated, function(req, res) {
     res.render("event", {
       layout: "cuser.handlebars"
     });
   });
-
    // /user/history route loads donation history of user
    app.get("/user/history", isAuthenticated, function(req, res) {
     res.render("dhistory", {
       layout: "user.handlebars"
     });
   });
-  
   // /cuser/history route loads event history of charity user
   app.get("/cuser/history", isAuthenticated, function(req, res) {
     res.render("ehistory", {
       layout: "cuser.handlebars"
     });
   });
-
   // list-events route loads list view of all events
   app.get("/list-events", function(req, res) {
     if (req.user) {
-      if(req.session.userType === "regular") {
-        res.render("listEvents", {
-          layout: "user.handlebars"
-        });
-      }
-      else if(req.session.userType === "charity") {
+      if(req.user.charityKey) {
         res.render("listEvents", {
           layout: "cuser.handlebars"
+        });
+      }
+      else {
+        res.render("listEvents", {
+          layout: "user.handlebars"
         });
       }
     }
@@ -120,18 +109,17 @@ module.exports = function(app) {
       res.render("listEvents");
     }
   });
-  
   // list-donations route loads list view of all donations
   app.get("/list-donations", function(req, res) {
     if (req.user) {
-      if(req.session.userType === "regular") {
-        res.render("listDonations", {
-          layout: "user.handlebars"
-        });
-      }
-      else if(req.session.userType === "charity") {
+      if(req.user.charityKey) {
         res.render("listDonations", {
           layout: "cuser.handlebars"
+        });
+      }
+      else {
+        res.render("listDonations", {
+          layout: "user.handlebars"
         });
       }
     }
@@ -139,13 +127,11 @@ module.exports = function(app) {
       res.render("listDonations");
     }
   });
-
   // Route for logging user out
   app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
   });
-
   // Route for getting some data about our user to be used client side
   app.get("/api/user_data", function(req, res) {
     if (!req.user) {
@@ -159,5 +145,3 @@ module.exports = function(app) {
     }
   });
 };
-
-  
