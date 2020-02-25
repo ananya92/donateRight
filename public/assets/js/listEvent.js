888beaf01194b37d1614e3eff355b50f4281c13c
ananya92 <1992ananya@gmail.com>
2020-02-25 18:41:27 +1030

    Added javascript code to load the selected event's details

---
+++ public/assets/js/listEvent.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    var eventList = $("#event-list");
+    var eventDetails = $("#event-details");
+    eventList.removeClass("is-hidden");
+    eventDetails.addClass("is-hidden");
+
+    $("li").on("click", function(event) {
+        event.preventDefault();
+        var id = $(this).data("id");
+        $.ajax({
+            method: "GET",
+            url: "/api/event/" + id,
+          }).then(function(data) {
+                console.log(data);
+                eventList.addClass("is-hidden");
+                eventDetails.removeClass("is-hidden");
+                $("#ename").text(data.name);
+                $("#edescription").text(data.description);
+                $("#cname").text(data.charityName);
+                $("#cdescription").text(data.charityDesc);
+                $("#email").text(data.contact);
+                $("#phnumber").text(data.email);
+        });
+    });
+
+    $("#backBtn").on("click", function() {
+        event.preventDefault();
+        location.reload();
+    });
+});
